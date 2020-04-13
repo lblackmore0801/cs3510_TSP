6c9cba7ce9b55be3422ce42b177d2efce1ec0fbc
Lindsey Blackmore <lblackmore0801@gmail.com>
2020-04-13 08:54:12 -0400

    added modularity and a mst_kruskals function that only sorts the edges of a graph

--- tsp-3510.py
+++ tsp-3510.py
@@ -1,5 +1,6 @@
 import sys
 import math
+import operator
 
 # argument parsing
 inputFile = sys.argv[1]
@@ -7,20 +8,42 @@ outputFile = sys.argv[2]
 time = sys.argv[3]
 
 coords = {}
-distances = {}
+
+def create_graph(coords):
+    distances = {}
+    for node1, coords1 in coords.items():
+        distances[node1] = {}
+        for node2, coords2 in coords.items():
+            distances[node1][node2] = dist(coords1, coords2)
+    return distances
 
 def dist(node1, node2):
     d = [node1[0] - node2[0], node1[1] - node2[1]]
     return round(math.sqrt(d[0] ** 2 + d[1] ** 2))
+
+def mst_kruskal(graph):
+    mst = []
+
+    if len(graph) == 0:
+        return []
+    if len(graph) == 1:
+        mst.append(graph[0])
+        return mst
+
+    edges = []
+    for vertex in graph:
+        for neighbor in graph[vertex]:
+            # avoid adding edges to self and two edges for each edge
+            if vertex != neighbor and vertex < neighbor:
+                edges.append((vertex,neighbor,graph[vertex][neighbor]))
+
+    edges.sort(key = operator.itemgetter(2))
+    print(edges)
 
 with open(inputFile, 'r') as reader:
     for line in reader.readlines():
         lineDataArr = line.split()
         coords[int(lineDataArr[0].replace('.',''))] = (float(lineDataArr[1]), float(lineDataArr[2]))
 
-for node1, coords1 in coords.items():
-    distances[node1] = {}
-    for node2, coords2 in coords.items():
-        distances[node1][node2] = dist(coords1, coords2) 
-
-print(distances)
+graph = create_graph(coords)
+mst_kruskal(graph)
